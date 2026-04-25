735b8617eb5a9bbf7e66560778f12d5cc3ba369b
Arunkg77 <arunkg77@gmail.com>
2026-04-25 11:43:28 +0530

    fdgh

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ import 'splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await Supabase.initialize(
-  //   url: 'https://xmhclankqtztjjpxgque.supabase.co',
-  //   anonKey: 'sb_publishable_9zhfvbKW-Av9gH5uYmidcA_Mn-BmhZx',
-  // );
+  await Supabase.initialize(
+    url: 'https://xmhclankqtztjjpxgque.supabase.co',
+    anonKey: 'sb_publishable_9zhfvbKW-Av9gH5uYmidcA_Mn-BmhZx',
+  );
 
   runApp(const MyApp());
 }
